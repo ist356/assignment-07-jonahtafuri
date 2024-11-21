9347f983d1fc6169d0fde6595299a88e71a30315
jttafuri@syr.edu <jttafuri@syr.edu>
2024-11-21 14:01:21 -0500

    progress!

--- code/menuitemextractor.py
+++ code/menuitemextractor.py
@@ -1,3 +1,5 @@
+from menuitem import MenuItem
+
 if __name__ == "__main__":
     import sys
     sys.path.append('code')
@@ -7,14 +9,19 @@ else:
 
 
 def clean_price(price:str) -> float:
-    pass # TODO  replace with code
+    clean_price = price.replace('$','')
+    return float(clean_price)
 
 def clean_scraped_text(scraped_text: str) -> list[str]:
+
     pass # TODO  replace with code
 
-def extract_menu_item(title:str, scraped_text: str) -> MenuItem:
-    pass # TODO  replace with code
-
+def extract_menu_item(item_category:str, item_price: str, item_name:str, item_desc:str) -> MenuItem:
+    menu_item = MenuItem(name = item_name, 
+                    price = item_price, 
+                    category= item_category, 
+                    description=item_desc)
+    return menu_item
 
 
 if __name__=='__main__':
